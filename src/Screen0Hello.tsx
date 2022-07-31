e41dc8023b62bb37c4fccfe76ad50b8442be7d3a
hopgoldy <hpgless@outlook.com>
2022-07-31 18:12:28 +0800

    fix: 首页无法点击

--- src/Screen0Hello.tsx
+++ src/Screen0Hello.tsx
@@ -10,7 +10,7 @@ export const Screen0Hello: Component = () => {
   }
 
   return (
-    <div class='h-screen text-center md:font-bold text-white relative' onClick={jumpToSecondScreen}>
+    <div class='h-screen text-center md:font-bold text-white relative z-20' onClick={jumpToSecondScreen}>
       <div class='absolute top-0 bottom-0 left-0 right-0 bg-black opacity-20'></div>
       <div class='select-none absolute top-1/3 z-10 w-screen text-6xl'>{appConfig.name}</div>
       <div class='absolute opacity-70 w-full flex items-center flex-col bottom-20 cursor-pointer animate-bounce'>
